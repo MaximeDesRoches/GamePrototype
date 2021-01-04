ed2cca808d5efed8196dbb9e0c48c4900a0160a8
Maxime Des Roches <desrochesmaxime30@gmail.com>
2021-01-03 23:23:03 -0500

    Redeem modal done

--- src/LastConnectionModal.js
+++ src/LastConnectionModal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { incrementByAmount, setLastConnection } from "./redux/points";
 import { lastConnectionPointsSelector } from "./selectors/points";
 
 const mapStateToProps = (state) => {
@@ -9,19 +10,32 @@ const mapStateToProps = (state) => {
 	};
 };
 
-function LastConnectionModal({ lastConnectionPoints }) {
+function LastConnectionModal({ lastConnectionPoints, add, setLC }) {
+	function onClick() {
+		add(lastConnectionPoints);
+		setLC(null);
+	}
+
 	return lastConnectionPoints > 0 ? (
 		<div className="last-connection-modal">
 			<div className="content">
 				Congrats !
 				<br />
 				While you were away you got {lastConnectionPoints} points!
-				<div className="upgrade-button">Redeem</div>
+				<div className="upgrade-button" onClick={onClick}>
+					Redeem
+				</div>
 			</div>
 		</div>
 	) : null;
 }
 
 export default connect(mapStateToProps, (dispatch) =>
-	bindActionCreators({}, dispatch)
+	bindActionCreators(
+		{
+			add: incrementByAmount,
+			setLC: setLastConnection
+		},
+		dispatch
+	)
 )(LastConnectionModal);
